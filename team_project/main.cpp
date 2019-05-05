5beaa52d38e959fa2794031aa7760adc1dc34a8e
Eric Miao <hongyum@smu.edu>
2019-05-05 10:59:58 -0500

    Optimize interface

--- team_project/main.cpp
+++ team_project/main.cpp
@@ -4,8 +4,16 @@ using namespace std;
 
 int main()
 {
+    string player_name1;
+    string player_name2;
+    cout<<"Welcome to Connect-4 With AI"<<endl;
+    cout<<"Player 1: Please type in your name: ";
+    cin>>player_name1;
+    cout<<"Player 2: Please type in your name: ";
+    cin>>player_name2;
     GameLauncher launchaer = GameLauncher(10,10);
-    launchaer.set_Players_names("Eric", "John");
+    launchaer.set_Players_names(player_name1,player_name2);
+    //launchaer.set_Players_names("Eric", "John");
     launchaer.set_Players_tokens("|O|", "|X|");
     launchaer.start_game();
     return 0;
